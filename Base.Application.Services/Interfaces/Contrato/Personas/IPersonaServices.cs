58d215ccd3c8adc7f3c227f189b9c7b36ecde2a4
Frank Rojas <101145147+FrankRojas31@users.noreply.github.com>
2025-03-24 00:37:55 -0500

    Fix: Se soluciona error en el backend que mostraba personas ya asociadas a un alumno

--- Base.Application.Services/Interfaces/Contrato/Personas/IPersonaServices.cs
+++ Base.Application.Services/Interfaces/Contrato/Personas/IPersonaServices.cs
@@ -1,9 +1,11 @@
 ﻿using Base.Domain.DTOs.Personas;
 using Base.Domain.Entidades.Personas;
+using Base.Domain.ViewModels;
 
 namespace Base.Application.Services.Interfaces.Contrato.Personas
 {
     public interface IPersonaServices : IServiceBase<PersonaEntity, PersonaEntityDTO>
     {
+        Task<ResponseHelper> GetPersonaSinRelacion();
     }
 }
--- Base.Application.Services/Interfaces/Implementacion/Personas/PersonaServices.cs
+++ Base.Application.Services/Interfaces/Implementacion/Personas/PersonaServices.cs
@@ -2,6 +2,7 @@
 using Base.Application.Services.Interfaces.Contrato.Personas;
 using Base.Domain.DTOs.Personas;
 using Base.Domain.Entidades.Personas;
+using Base.Domain.ViewModels;
 using Base.Infraestructura.Data.Repositorios.Contrato;
 using Base.Infraestructura.Data.Repositorios.Contrato.Personas;
 using System;
@@ -15,9 +16,45 @@ namespace Base.Application.Services.Interfaces.Implementacion.Personas
     public class PersonaServices : ServiceBase<PersonaEntity, PersonaEntityDTO>, IPersonaServices
     {
         private readonly IPersonaRepository _personaRepository;
-        public PersonaServices(IMapper mapper, IPersonaRepository personaRepository) : base(mapper, personaRepository)
+        private readonly IAlumnoRepository _alumnoRepository;
+        public PersonaServices(IMapper mapper, IPersonaRepository personaRepository, IAlumnoRepository alumnoRepository) : base(mapper, personaRepository)
         {
             _personaRepository = personaRepository;
+            _alumnoRepository = alumnoRepository;
+        }
+
+        public async Task<ResponseHelper> GetPersonaSinRelacion()
+        {
+            try
+            {
+                List<AlumnoEntity> listaAlumno = await _alumnoRepository.GetAllAsync();
+                List<PersonaEntity> listaPersona = await _personaRepository.GetAllAsync();
+                List<PersonaEntity> lista = [];
+
+                foreach (PersonaEntity persona in listaPersona)
+                {
+                    if (listaAlumno.Any(x => x.IdPersona == persona.Id))
+                        continue;
+                    else
+                        lista.Add(persona);
+                }
+
+                return new ResponseHelper
+                {
+                    Success = true,
+                    Message = "Lista de Personas servida correctamente",
+                    Data = lista
+                };
+            }
+            catch (Exception ex)
+            {
+
+                return new ResponseHelper
+                {
+                    Success = false,
+                    Message = ex.Message,
+                };
+            }
         }
     }
 }
